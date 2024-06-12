2257d1ee02db06e991974c4cf1c133dbc6bb5451
dzemil karisik <karisikdzemil@gmail.com>
2024-06-12 20:53:48 +0200

    first code

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP-C++
+//
+//  Created by dzemil karisik on 12.6.24..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
