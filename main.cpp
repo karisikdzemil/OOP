f0ba1dcbf9cecd4064689bdd11982a9f99f10c72
dzemil karisik <karisikdzemil@gmail.com>
2024-06-13 13:49:42 +0200

    Costructors

--- main.cpp
+++ main.cpp
@@ -2,23 +2,39 @@
 using std:: string;
 
 
-
-
-class Emloyee {
+class Employee {
 public:
     string Name;
     string Company;
     int Age;
+    
+    
+    void IntroduceYourself (){
+        std::cout<< "Name - " << Name <<std::endl;
+        std::cout<< "Company - " << Company <<std::endl;
+        std::cout<< "Age - " << Age <<std::endl;
+    }
+    
+    Employee(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+    }
 };
+
 
 
 int main() {
     
-    Emloyee Employe1;
-    Employe1.Name = "Saldina";
-    Employe1.Company = "YT-CodeBeauty";
-    Employe1.Age = 25;
-   
+    Employee employe1 = Employee("Saldina", "YT-CodeBeauty", 25);
+//    employe1.Name = "Saldina";
+//    employe1.Company = "YT-CodeBeauty";
+//    employe1.Age = 25   ;
+    employe1.IntroduceYourself();
     
-    
+    Employee employe2 = Employee("John", "Amazon", 35);
+//    employe1.Name = "Saldina";
+//    employe1.Company = "YT-CodeBeauty";
+//    employe1.Age = 25   ;
+    employe2.IntroduceYourself();
 }
