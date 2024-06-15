6bffdcbcb10a986458df3227cb95f3bf314b269e
dzemil karisik <karisikdzemil@gmail.com>
2024-06-15 15:56:02 +0200

    basic

--- main.cpp
+++ main.cpp
@@ -123,132 +123,196 @@
 
 // Zadatak 1: Napraviti sistem za upravljanje bibliotekom knjiga koristeći klase i objekte u C++.
 
+//
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//class Knjiga {
+//private:
+//    string naslov;
+//    string autor;
+//    int godinaIzdanja;
+//    string ISBN;
+//    bool dostupna;
+//
+//public:
+//    Knjiga(string nasl, string aut, int god, string isbn)
+//        : naslov(nasl), autor(aut), godinaIzdanja(god), ISBN(isbn), dostupna(true) {}
+//
+//    void setNaslov(string nasl) { naslov = nasl; }
+//    string getNaslov() { return naslov; }
+//
+//    void setAutor(string aut) { autor = aut; }
+//    string getAutor() { return autor; }
+//
+//    void setGodinaIzdanja(int god) { godinaIzdanja = god; }
+//    int getGodinaIzdanja() { return godinaIzdanja; }
+//
+//    void setISBN(string isbn) { ISBN = isbn; }
+//    string getISBN() { return ISBN; }
+//
+//    void setDostupna(bool dos) { dostupna = dos; }
+//    bool isDostupna() { return dostupna; }
+//
+//    void prikaziInformacije() {
+//        cout << "Naslov: " << naslov << endl;
+//        cout << "Autor: " << autor << endl;
+//        cout << "Godina izdanja: " << godinaIzdanja << endl;
+//        cout << "ISBN: " << ISBN << endl;
+//        cout << "Dostupna: " << (dostupna ? "Da" : "Ne") << endl;
+//    }
+//
+//    void iznajmiKnjigu() {
+//        if (dostupna) {
+//            dostupna = false;
+//            cout << "Knjiga " << naslov << " je iznajmljena." << endl;
+//        } else {
+//            cout << "Knjiga " << naslov << " nije dostupna za iznajmljivanje." << endl;
+//        }
+//    }
+//
+//    void vratiKnjigu() {
+//        dostupna = true;
+//        cout << "Knjiga " << naslov << " je vracena." << endl;
+//    }
+//};
+//
+//class Biblioteka {
+//private:
+//    vector<Knjiga> knjige;
+//
+//public:
+//    void dodajKnjigu(Knjiga novaKnjiga) {
+//        knjige.push_back(novaKnjiga);
+//    }
+//
+//    void prikaziSveKnjige() {
+//        for (Knjiga &knjiga : knjige) {
+//            knjiga.prikaziInformacije();
+//            cout << "-------------------------" << endl;
+//        }
+//    }
+//
+//    void prikaziDostupneKnjige() {
+//        for (Knjiga &knjiga : knjige) {
+//            if (knjiga.isDostupna()) {
+//                knjiga.prikaziInformacije();
+//                cout << "-------------------------" << endl;
+//            }
+//        }
+//    }
+//
+//    void iznajmiKnjigu(string ISBN) {
+//        for (Knjiga &knjiga : knjige) {
+//            if (knjiga.getISBN() == ISBN) {
+//                knjiga.iznajmiKnjigu();
+//                return;
+//            }
+//        }
+//        cout << "Knjiga sa ISBN " << ISBN << " nije pronadjena." << endl;
+//    }
+//
+//    void vratiKnjigu(string ISBN) {
+//        for (Knjiga &knjiga : knjige) {
+//            if (knjiga.getISBN() == ISBN) {
+//                knjiga.vratiKnjigu();
+//                return;
+//            }
+//        }
+//        cout << "Knjiga sa ISBN " << ISBN << " nije pronadjena." << endl;
+//    }
+//};
+//
+//int main() {
+//    Biblioteka biblioteka;
+//    Knjiga k1("Na Drini Cuprija", "Ivo Andric", 1945, "123456789");
+//    Knjiga k2("Prokleta avlija", "Ivo Andric", 1954, "987654321");
+//
+//    biblioteka.dodajKnjigu(k1);
+//    biblioteka.dodajKnjigu(k2);
+//
+//    cout << "Sve knjige u biblioteci:" << endl;
+//    biblioteka.prikaziSveKnjige();
+//
+//    cout << "\nDostupne knjige u biblioteci:" << endl;
+//    biblioteka.prikaziDostupneKnjige();
+//
+//    cout << "\nIznajmljivanje knjige sa ISBN 123456789:" << endl;
+//    biblioteka.iznajmiKnjigu("123456789");
+//
+//    cout << "\nDostupne knjige nakon iznajmljivanja:" << endl;
+//    biblioteka.prikaziDostupneKnjige();
+//
+//    cout << "\nVracanje knjige sa ISBN 123456789:" << endl;
+//    biblioteka.vratiKnjigu("123456789");
+//
+//    cout << "\nDostupne knjige nakon vracanja:" << endl;
+//    biblioteka.prikaziDostupneKnjige();
+//
+//    return 0;
+//}
 
-#include <iostream>
-#include <vector>
-using namespace std;
+//ZADATAK 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-class Knjiga {
-private:
-    string naslov;
-    string autor;
-    int godinaIzdanja;
-    string ISBN;
-    bool dostupna;
+//#include <iostream>
+//using std:: string;
+//
+//class Automobil {
+//private:
+//    string Marka;
+//    string Model;
+//    int GodProizvodnje;
+//    double Cena;
+//public:
+//    Automobil (string marka, string model, int godProizvodnje, double cena){
+//        Marka=marka;
+//        Model=model;
+//        GodProizvodnje=godProizvodnje;
+//        Cena=cena;
+//    }
+//    void setMarka (string marka){
+//        Marka = marka;
+//    }
+//    string getMarka (){
+//        return Marka;
+//    }
+//    void setModel (string model){
+//        Model = model;
+//    }
+//    string getModel (){
+//        return Model;
+//    }
+//    void setGodProizvodnje (int godProizvodnje){
+//        GodProizvodnje=godProizvodnje;
+//    }
+//    int getGodProizvodnje (){
+//        return GodProizvodnje;
+//    }
+//    void setCena (int cena){
+//        Cena = cena;
+//    }
+//    int getCena (){
+//        return Cena;
+//    }
+//    void PrikaziPodatke() {
+//        std::cout<< "\nMarka- "<< Marka<< "\nModel- "<< Model<<"\nGodina proizvodnje- "<<GodProizvodnje<<"\nCena- "<<Cena<<std::endl;
+//    };
+//};
+//
+//int main (){
+//    
+//    Automobil automobil1 = Automobil("VW", "Golf", 2024, 67500.23);
+//    Automobil automobil2 = Automobil("BMW", "M3", 2022, 85000.50);
+//    Automobil automobil3 = Automobil("Mercedes", "S-class", 2023, 112000);
+//    automobil1.PrikaziPodatke();
+//    automobil2.PrikaziPodatke();
+//    automobil3.PrikaziPodatke();
+//    
+//    automobil1.setCena(50000);
+//    automobil1.PrikaziPodatke();
+//    return 0;
+//}
 
-public:
-    Knjiga(string nasl, string aut, int god, string isbn)
-        : naslov(nasl), autor(aut), godinaIzdanja(god), ISBN(isbn), dostupna(true) {}
 
-    void setNaslov(string nasl) { naslov = nasl; }
-    string getNaslov() { return naslov; }
 
-    void setAutor(string aut) { autor = aut; }
-    string getAutor() { return autor; }
-
-    void setGodinaIzdanja(int god) { godinaIzdanja = god; }
-    int getGodinaIzdanja() { return godinaIzdanja; }
-
-    void setISBN(string isbn) { ISBN = isbn; }
-    string getISBN() { return ISBN; }
-
-    void setDostupna(bool dos) { dostupna = dos; }
-    bool isDostupna() { return dostupna; }
-
-    void prikaziInformacije() {
-        cout << "Naslov: " << naslov << endl;
-        cout << "Autor: " << autor << endl;
-        cout << "Godina izdanja: " << godinaIzdanja << endl;
-        cout << "ISBN: " << ISBN << endl;
-        cout << "Dostupna: " << (dostupna ? "Da" : "Ne") << endl;
-    }
-
-    void iznajmiKnjigu() {
-        if (dostupna) {
-            dostupna = false;
-            cout << "Knjiga " << naslov << " je iznajmljena." << endl;
-        } else {
-            cout << "Knjiga " << naslov << " nije dostupna za iznajmljivanje." << endl;
-        }
-    }
-
-    void vratiKnjigu() {
-        dostupna = true;
-        cout << "Knjiga " << naslov << " je vracena." << endl;
-    }
-};
-
-class Biblioteka {
-private:
-    vector<Knjiga> knjige;
-
-public:
-    void dodajKnjigu(Knjiga novaKnjiga) {
-        knjige.push_back(novaKnjiga);
-    }
-
-    void prikaziSveKnjige() {
-        for (Knjiga &knjiga : knjige) {
-            knjiga.prikaziInformacije();
-            cout << "-------------------------" << endl;
-        }
-    }
-
-    void prikaziDostupneKnjige() {
-        for (Knjiga &knjiga : knjige) {
-            if (knjiga.isDostupna()) {
-                knjiga.prikaziInformacije();
-                cout << "-------------------------" << endl;
-            }
-        }
-    }
-
-    void iznajmiKnjigu(string ISBN) {
-        for (Knjiga &knjiga : knjige) {
-            if (knjiga.getISBN() == ISBN) {
-                knjiga.iznajmiKnjigu();
-                return;
-            }
-        }
-        cout << "Knjiga sa ISBN " << ISBN << " nije pronadjena." << endl;
-    }
-
-    void vratiKnjigu(string ISBN) {
-        for (Knjiga &knjiga : knjige) {
-            if (knjiga.getISBN() == ISBN) {
-                knjiga.vratiKnjigu();
-                return;
-            }
-        }
-        cout << "Knjiga sa ISBN " << ISBN << " nije pronadjena." << endl;
-    }
-};
-
-int main() {
-    Biblioteka biblioteka;
-    Knjiga k1("Na Drini Cuprija", "Ivo Andric", 1945, "123456789");
-    Knjiga k2("Prokleta avlija", "Ivo Andric", 1954, "987654321");
-
-    biblioteka.dodajKnjigu(k1);
-    biblioteka.dodajKnjigu(k2);
-
-    cout << "Sve knjige u biblioteci:" << endl;
-    biblioteka.prikaziSveKnjige();
-
-    cout << "\nDostupne knjige u biblioteci:" << endl;
-    biblioteka.prikaziDostupneKnjige();
-
-    cout << "\nIznajmljivanje knjige sa ISBN 123456789:" << endl;
-    biblioteka.iznajmiKnjigu("123456789");
-
-    cout << "\nDostupne knjige nakon iznajmljivanja:" << endl;
-    biblioteka.prikaziDostupneKnjige();
-
-    cout << "\nVracanje knjige sa ISBN 123456789:" << endl;
-    biblioteka.vratiKnjigu("123456789");
-
-    cout << "\nDostupne knjige nakon vracanja:" << endl;
-    biblioteka.prikaziDostupneKnjige();
-
-    return 0;
-}
